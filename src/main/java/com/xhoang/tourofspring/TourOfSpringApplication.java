b11d1571e8efdaa06c28d5df4ab5f1ea4e36426c
hoang14x <hoang.nguyen14@tiki.vn>
2023-10-26 11:47:42 +0700

    first commit

---
+++ src/main/java/com/xhoang/tourofspring/TourOfSpringApplication.java
@@ -0,0 +1,13 @@
+
+package com.xhoang.tourofspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourOfSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TourOfSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xhoang/tourofspring/TourOfSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xhoang.tourofspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourOfSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
